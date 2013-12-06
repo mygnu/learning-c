ad44c6a166401c7cd0ec1392737c94285abb799e
mygnu <harman@mygnu.org>
2013-12-06 23:37:05 +1030

    new commit

---
+++ aaprogram.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+    char card_name[3];
+	puts("Enter the cord_name: ");
+	scanf("%2s", card_name);
+	int val = 0;
+	
+	if ((card_name[0] == 'K') ||
+		(card_name[0] == 'Q') ||
+		(card_name[0] == 'J'))
+		{
+			val = 10;
+		} else if (card_name[0] == 'A')
+		{
+			val = 11;
+		}
+	else
+		{
+			val=atoi(card_name);
+		}
+	
+	if ((val >= 3) && (val <= 6))
+		{
+			puts("Count has gone UP!");
+		} else if ((val >= 10))
+		{
+			puts("Count has gone DOWN!");
+		}
+	
+
+	return 0;
+}
