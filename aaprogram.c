faba21e7de020f2dc4df8cbeee17ea9becf27b25
mygnu <harman@mygnu.org>
2013-12-17 11:27:44 +1030

    include a collcetive header

--- aaprogram.c
+++ aaprogram.c
@@ -1,31 +1,10 @@
-/* Given the length and breadth of a rectangle, write a program to */
-/* find whether the area of the rectangle is greater than its */
-/* perimeter. For example, the area of the rectangle with length = 5 */
-/* and breadth = 4 is greater than its perimeter. */
-
-
-
-#include <stdio.h>
-
+#include "header.h"
+ 
 int main(int argc, char *argv[])
 {
-	int len,wid,area,per;
+	char a[] = "Hello World";
 
-	printf("Please the legth and width of a rectangle: ");
-
-	scanf("%d %d", &len,&wid);
-
-	area = len * wid;
-	per = len * 2 + wid * 2;
-	
-	if (area > per)
-		{
-			printf ("The area %d of the rectangle is greater than the prerimeter %d \n",area, per);
-		}
-	else
-		printf ("The area %d of the rectangle is smaller than the prerimeter %d \n",area, per);
-	
-	
+	printf ("%s\n",a);
 	return 0;
 }
 
---
+++ ch2_j.c
@@ -0,0 +1,23 @@
+/* Given three points (x1, y1), (x2, y2) and (x3, y3), write a */
+/* program to check if all the three points fall on one straight line. */
+
+ 
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	float x1,x2,x3,y1,y2,y3;
+
+	printf("\nEnter the three coordinates (x1,y1),(x2,y2),(x3,y3)");
+
+	scanf("%f%f%f%f%f%f",&x1,&y1,&x2,&y2,&x3,&y3);
+
+	if ((y2-y1)/(x2-x1)==(y3-y2)/(x3-x2))
+		printf("\nThe three points fall on the same line\n");
+	else
+		printf("\nThe three points are not collinear \n");
+	
+	return 0;
+}
+
