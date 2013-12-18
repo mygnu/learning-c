bf876a9cef54c7dad816b9f10aec96c07057ad56
mygnu <harman@mygnu.org>
2013-12-18 15:25:00 +1030

    example g

--- ch2f_g.c
+++ ch2f_g.c
@@ -1,7 +1,7 @@
 /* If the three sides of a triangle are entered through the */
 /* keyboard, write a program to check whether the triangle is */
-/* valid or not. The triangle is valid if the sum of two sides is */
-/* greater than the largest of the three sides. */
+/* isosceles, equilateral, scalene or right angled triangle. */
+
 
 
 
