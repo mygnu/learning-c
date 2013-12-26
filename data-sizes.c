8641f475e3947bf124d7e03154177a30a29a059a
mygnu <harman@mygnu.org>
2013-12-26 12:09:56 +1030

    Armstrong Numbers

--- data-sizes.c
+++ data-sizes.c
@@ -3,14 +3,13 @@
  
 int main(int argc, char *argv[])
 {
+	printf("sizeof(char) == %d\n", sizeof(char));
+	printf("sizeof(short) == %d\n", sizeof(short));
+	printf("sizeof(int) == %d\n", sizeof(int));
+	printf ("%s\n",);("sizeof(long) == %d\n", sizeof(long));
+	printf("sizeof(long long) == %d\n", sizeof(long long));
 	printf("Storage size for float : %d \n", sizeof(float));
-	printf("Storage size for int : %d \n", sizeof(int));
-	printf("Storage size for long : %d \n", sizeof(long));
 	printf("Storage size for double : %d \n", sizeof(double));
-	printf("Storage size for long long : %d \n", sizeof(long long));
-	printf("Storage size for char : %d \n", sizeof(char));
-	
-	
 	printf("Minimum float positive value: %E\n", FLT_MIN );
 	printf("Maximum float positive value: %E\n", FLT_MAX );
 	printf("Precision value: %d\n", FLT_DIG );
