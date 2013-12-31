3fc24aaf8cf33e7dca8edb8f768506e4d7cc15cf
mygnu <harman@mygnu.org>
2013-12-31 10:34:52 +1030

    minor changes

--- aaprogram.c
+++ aaprogram.c
@@ -1,36 +1,17 @@
-/* recursive functions */
+/* more functions */
+
+
+
+//  getfrmkbd(&x);
 
 #include "header.h"
-
-void getfrmkbd( int *k)
-{
-	printf("Please enter a number: ");
-	scanf("%d", &(*k));
-}
-
-void primefactors(int x)
-	{
-		int N = x;
-		for (int i = 2; i < N; i++)
-			{
-				if (x%i==0)
-					{
-						printf("%d ", i);
-						x = x/i;
-						i=1;
-					}
-			}
-		printf("\n");
-		
-
-	}
 
 int 
 main(int argc, char *argv[])
 {
-	int x;
-	getfrmkbd(&x);
-	primefactors(x);
-	return 0;
+  int x;
+  getfrmkbd(&x);
+  primefactors(x);
+  return 0;
 }
 
--- myfun.c
+++ myfun.c
@@ -2,72 +2,72 @@
 
 double power(int *x, int *y)
 {
-	double res =0;
-	for (int i = 1; i < *y; i++)
-		{
-			res = res * (*x);
-		}
-	return (res);
+  double res =0;
+  for (int i = 1; i < *y; i++)
+	{
+	  res = res * (*x);
+	}
+  return (res);
 }
 
 
 void fabonacci(int n)
 {
-	unsigned long n1 = 0;
-	unsigned long n2 = 1;
-	printf("%lu, %lu", n1,n2);
+  unsigned long n1 = 0;
+  unsigned long n2 = 1;
+  printf("%lu, %lu", n1,n2);
 	
 	
-	for (int i = 2; i < n; i++)
-		{
-			unsigned long n3 = n1 + n2;
-			printf(", %lu",n3);
-			n1 = n2;
-			n2 = n3;
-		}
-	printf("\n");
+  for (int i = 2; i < n; i++)
+	{
+	  unsigned long n3 = n1 + n2;
+	  printf(", %lu",n3);
+	  n1 = n2;
+	  n2 = n3;
+	}
+  printf("\n");
 }
 
 
 int factorial(int x) // recursive function
 {
-	int f;
-	if(x==1)
-		return(1);
-	else
-		f=x*factorial(x-1);
-	return (f);
+  int f;
+  if(x==1)
+	return(1);
+  else
+	f=x*factorial(x-1);
+  return (f);
 }
 
 unsigned int sumofn( unsigned int n)
 {
-	if(n == 0)
-		return 0;
-	return n%10 + sumofn(n/10);
+  if(n == 0)
+	return 0;
+  return n%10 + sumofn(n/10);
 }
 
 
 
 void getfrmkbd( int *k)
 {
-	printf("Please enter a number: ");
-	scanf("%d", &(*k));
+  printf("Please enter a number: ");
+  scanf("%d", &(*k));
 }
 
 void primefactors(int x)
+{
+  int N = x;
+  for (int i = 2; i < N; i++)
 	{
-		int N = x;
-		for (int i = 2; i < N; i++)
-			{
-				if (x%i==0)
-					{
-						printf("%d ", i);
-						x = x/i;
-						i=1;
-					}
-			}
-		printf("\n");
+	  if (x%i==0)
+		{
+		  printf("%d ", i);
+		  x = x/i;
+		  i=1;
+		}
 	}
+  printf("\n");
+}
 
 
 
