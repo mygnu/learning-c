35073f5f78c035236a86891b64bc830cdca89af7
mygnu <harman@mygnu.org>
2014-01-01 21:39:31 +1030

    data types re-visited

--- aaprogram.c
+++ aaprogram.c
@@ -1,46 +1,22 @@
-/*  A positive integer is entered through the keyboard, write a */
-/* function to find the binary equivalent of this number using */
-/* recursion. */
-
 
 //  getfrmkbd(&x);
 
 #include "header.h"
 
- #include<stdio.h>
+int *fun()
+  {
+    int k = 35;
+    return (&k);
+  }
 
+int 
+main(int argc, char *argv[])
+{
+  int *j;
+  j = fun();
+  printf("%i\n",*j);
+  
+  
 
-int main(){
-
-    long int decimalNumber, quotient;
-
-    int binaryNumber[100],i=1,j;
-
-
-    printf("Enter any decimal number: ");
-
-    scanf("%ld",&decimalNumber);
-
-
-    quotient = decimalNumber;
-
-
-    while(quotient!=0){
-
-         binaryNumber[i++]= quotient % 2;
-
-         quotient = quotient / 2;
-
-    }
-
-
-    printf("Equivalent binary value of decimal number %d: ",decimalNumber);
-
-    for(j = i -1 ;j> 0;j--)
-
-         printf("%d",binaryNumber[j]);
-    printf("\n");
-
-    return 0;
-
+  return 0;
 }
--- data-sizes.c
+++ data-sizes.c
@@ -1,22 +1,23 @@
 
-#include "header.h" //gets most common header files from my own header
- 
-int main(int argc, char *argv[])
+
+//  getfrmkbd(&x);
+#include "header.h"
+
+
+int 
+main(int argc, char *argv[])
 {
-	printf("sizeof(char) == %d\n", sizeof(char));
-	printf("sizeof(short) == %d\n", sizeof(short));
-	printf("sizeof(int) == %d\n", sizeof(int));
-	printf ("%s\n",);("sizeof(long) == %d\n", sizeof(long));
-	printf("sizeof(long long) == %d\n", sizeof(long long));
-	printf("Storage size for float : %d \n", sizeof(float));
-	printf("Storage size for double : %d \n", sizeof(double));
+  	printf("sizeof(char) == %lu\n", sizeof(char));
+	printf("sizeof(short) == %lu\n", sizeof(short));
+	printf("sizeof(int) == %lu\n", sizeof(int));
+	printf("sizeof(long long) == %lu\n", sizeof(long long));
+	printf("Storage size for float : %lu \n", sizeof(float));
+	printf("Storage size for double : %lu \n", sizeof(double));
+	printf("Storage size for long double : %lu \n", sizeof(long double));
 	printf("Minimum float positive value: %E\n", FLT_MIN );
 	printf("Maximum float positive value: %E\n", FLT_MAX );
 	printf("Precision value: %d\n", FLT_DIG );
 	
-   
+	  
 	return 0;
 }
-
-
-
