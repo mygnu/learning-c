883e66a50fa1860de06efda3532aa8870bba8db7
mygnu <harman@mygnu.org>
2014-01-19 14:25:03 +1030

    comments and linux style indentation

--- myfun.c
+++ myfun.c
@@ -3,76 +3,76 @@
 /* take two ints raises second to the power of first */
 double power(int x, int y)
 {
-  double res =1;
-  for (int i = 0; i < y; i++)
-	{
-	  res = res * (x);
-	}
-  return (res);
+    double res =1;
+    for (int i = 0; i < y; i++)
+    {
+        res = res * (x);
+    }
+    return (res);
 }
 
 /* takes an int and pirnts fabonacci series up to that */
 void fabonacci(int n)
 {
-  unsigned long n1 = 0;
-  unsigned long n2 = 1;
-  printf("%lu, %lu", n1,n2);
-	
-	
-  for (int i = 2; i < n; i++)
-	{
-	  unsigned long n3 = n1 + n2;
-	  printf(", %lu",n3);
-	  n1 = n2;
-	  n2 = n3;
-	}
-  printf("\n");
+    unsigned long n1 = 0;
+    unsigned long n2 = 1;
+    printf("%lu, %lu", n1,n2);
+
+
+    for (int i = 2; i < n; i++)
+    {
+        unsigned long n3 = n1 + n2;
+        printf(", %lu",n3);
+        n1 = n2;
+        n2 = n3;
+    }
+    printf("\n");
 }
 
 /* takes an int and calculates the factorial value of that int */
 /* returns int */
 int factorial(int x) // recursive function
 {
-  int f;
-  if(x==1)
-	return(1);
-  else
-	f=x*factorial(x-1);
-  return (f);
+    int f;
+    if(x==1)
+        return(1);
+    else
+        f=x*factorial(x-1);
+    return (f);
 }
 
 /* takes an unsigned int and calculates the sum of all the digits */
 /* returns unsigned int */
 unsigned int sumofn( unsigned int n)
 {
-  if(n == 0)
-	return 0;
-  return n%10 + sumofn(n/10);
+    if(n == 0)
+        return 0;
+    return n%10 + sumofn(n/10);
 }
 
 
 /* prints the pormpt to take an pointer to integer and from the keyboard and
- assignns the value at the address of int */
+   assignns the value at the address of int */
 void getfrmkbd( int *k)
 {
-  printf("Please enter a number: ");
-  scanf("%d", &(*k));
+    printf("Please enter a number: ");
+    scanf("%d", &(*k));
 }
 
 /* takes an int and prints pirme factors of that int */
 void primefactors(int x)
 {
-  int N = x;
-  for (int i = 2; i < N; i++)
-	{
-	  if (x%i==0)
-		{
-		  printf("%d ", i);
-		  x = x/i;
-		  i=1;
-		}
-	}
-  printf("\n");
+    int N = x;
+    for (int i = 2; i < N; i++)
+    {
+        if (x%i==0)
+        {
+            printf("%d ", i);
+            x = x/i;
+            i=1;
+        }
+    }
+    printf("\n");
 }
 
 /* takes an int and prints sum of all its digits */
@@ -91,7 +91,3 @@ void primefactors(int x)
 /*     else */
 /*     return ( sum ); */
 /* } */
-
-
-
-
